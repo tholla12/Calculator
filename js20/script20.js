c78966d118f4885ae3707138c3e31328fbf3d1f6
Tola Bello <tolabello4@gmail.com>
2023-02-03 16:55:53 -0800

    javascript file

--- js20/script20.js
+++ js20/script20.js
@@ -1,4 +1,19 @@
-
+// Business logic .
+function add(number1, number2) {
+  return (number1 + number2)
+}
+function subtract(number1, number2) {
+  return (number1 - number2)
+}
+function multiply(number1, number2) {
+  return (number1 * number2)
+}
+function divide(number1, number2) {
+  return (number1 / number2)
+}
+function modulus(number1, number2) {
+  return (number1 % number2)
+}
 // User interface logic.
 $(document).ready(function () {
   $("form#calculator").submit(function (event) {
